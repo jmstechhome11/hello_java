f9aff9c6116c3e7b8581ffe3d60c9e541c14cb43
jmstechhome11 <jmstechhome11@gmail.com>
2021-07-27 16:02:41 +0000

    added some commits

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("tuesday");
+    System.out.println("hi friend");
+    System.out.println("bye friend");
     
   }
 }
